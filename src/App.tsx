4254ec30d6144fbb0479cda398fc7fae96011ebc
Son Bui <33485364+nsonb@users.noreply.github.com>
2021-03-28 17:22:53 +0300

    alternating image hook and react-palette in use

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import Header from './components/Header'
 import Footer from './components/Footer'
 import Intro from './components/Intro'
 
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,29 +1,45 @@
 import styled from 'styled-components'
+import { usePalette } from 'react-palette'
+import { useAlternatingImage } from '../hook/useAlternatingImage'
+
+// images
 import opt from '../img/jap_opt.jpg'
 import demonic_tutor from '../img/nihonga_Demonic_Tutor_art.jpg'
 import sword from '../img/nihonga_Swords_to_Plowshares_art.jpg'
 
 const IntroContainer = styled.div`
   overflow: hidden;
-  width: 100%;
+  width: 80%;
   position: relative;
-  height: 100vh;
+  height: 80vh;
+  border-radius: .5rem;
+  margin: 2rem auto;
+`
 
-  & img {
-    position: absolute;
-    left: 0;
-    top: -12%;
-    width: 100vw;
-    object-fit: cover;
-    overflow: hidden;
-  }
+const Image = styled.img`
+  position: absolute;
+  left: 0;
+  top: -10rem;
+  width: 100%;  
+  object-fit: cover;
+  overflow: hidden;
+  transition: all 1s ease;
 `
 
 const Intro = () => {
-  const card_img = [ opt , demonic_tutor, sword ]
+  const card_imgs = [ opt , demonic_tutor, sword ]
+
+  const { current } = useAlternatingImage(0, card_imgs, 8000)
+  
+  const { data, loading, error } = usePalette(card_imgs[current])
+
   return (
     <IntroContainer>
-      <img src={card_img[1]} alt="japanese art for opt"/>
+      { card_imgs.map((img, index) => {
+        return (
+          <Image src={img} alt="deco" key={img+index} style={index === current? {opacity: '1'} : {opacity: '0'}}/>
+        )
+      })}
     </IntroContainer>
   )
 }
